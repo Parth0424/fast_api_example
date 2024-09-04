8e6d3b3ba0fac6ffe85eae49735fce6165f2d49e
Parth0424 <parthkumar.patel@einfochips.com>
2024-09-04 11:12:08 +0530

    good

--- task_1/task1.py
+++ task_1/task1.py
@@ -34,7 +34,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 SECRET_KEY = "secret"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
+# jii
+# jiijij
+# jiji
 # Password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
